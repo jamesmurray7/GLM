6c612f024c8cf5c0f29a65fb75a4544d96d0ace1
jamesmurray7 <j.murray7@ncl.ac.uk>
2021-10-19 17:19:19 +0100

    for tomorrow

--- Poisson/testing/full-test.R
+++ Poisson/testing/full-test.R
@@ -1,3 +1,8 @@
+library(survival)
+library(dplyr)
+library(Rcpp)
+library(RcppArmadillo)
+
 rm(list=ls())   
 vech <- function(x) x[lower.tri(x, diag = T)]
 sourceCpp('Poisson/ll.cpp')
@@ -7,8 +12,8 @@ source('./DataFunctions/survFns.R')
 
 # Simulate some Data
 args(simData)
-beta <- rbind(c(10, 0.01, -0.05, 1),
-              c(10, -0.05, 0.1, 0.8))
+beta <- rbind(c(0.75, -0.05, -0.2, 0.2),
+              c(1, 0.05,  0.5, -0.80))
 D <- diag(4)
 D[1, 1] <- D[3, 3] <- 0.5^2
 D[2, 2] <- D[4, 4] <- 0.2^2
@@ -18,6 +23,8 @@ gamma <- c(-0.5, 0.8)
 eta <- c(-0.3, 0.5)
 
 data <- simData(250, 10, beta, D, gamma, eta, theta = c(-3, 0.2)) # appx. 50%
+# Check all ids actually here, if not just rerun line above
+length(unique(data$id))
 ph <- coxph(Surv(survtime, status) ~ cont + bin, data = dplyr::distinct(data, id, cont, bin, survtime, status))
 
 X <- getXi(data, 2)
@@ -25,20 +32,10 @@ Y <- getYi(data, 2)
 Z <- getZi(data, 2)
 mi <- getmi(data, 2) # Dont actually think this will be needed(!)
 
-
-# Initial conditions
+#' Initial conditions -----
 source('inits/inits.R')
-inits.long <- suppressMessages(suppressWarnings(Longit.inits(2, data)))
+inits.long <- Longit.inits(2, data)
 inits.surv <- TimeVarCox(data, Ranefs(inits.long))
-
-source('./inits/MVLME.R')
-mvlme.fit <- mvlme(data, Y, X, Z, inits.long, mvlme.tol = 5e-3)
-
-beta <- mvlme.fit$beta.init
-D <- mvlme.fit$D.init
-b <- mvlme.fit$b # currently a list
-gamma <- inits.surv$inits[3:4]
-eta <- inits.surv$inits[1:2]
 
 sv <- surv.mod(ph, data, inits.surv$l0.init)
 # Extract all survival-related objects
@@ -48,3 +45,20 @@ Di <- sv$Di
 l0 <- sv$l0; l0i <- sv$l0i; l0u <- sv$l0u
 Fi <- sv$Fi; Fu <- sv$Fu
 K <- getKi(data, ph)
+
+#' MVLME Step ----
+source('./inits/MVLME.R')
+mvlme.fit <- mvlme(data, Y, X, Z, inits.long, nK = 2, q = 4,  
+                   mvlme.tol = 5e-3)
+
+beta <- mvlme.fit$beta.init
+D <- mvlme.fit$D.init
+b <- mvlme.fit$b # currently a list
+gamma <- inits.surv$inits[3:4]
+eta <- inits.surv$inits[1:2]
+
+
+gh <- statmod::gauss.quad.prob(9, 'normal')
+w <- gh$w; v <- gh$n
+
+
