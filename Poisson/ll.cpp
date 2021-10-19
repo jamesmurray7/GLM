1c7b17e08829733abe8a60f5666f248db3c339e6
jamesmurray7 <j.murray7@ncl.ac.uk>
2021-10-19 17:18:13 +0100

    Expanding ll.cpp to include po_ll, po_grad and po_hess

--- Poisson/ll.cpp
+++ Poisson/ll.cpp
@@ -15,13 +15,38 @@ double ll(vec& b, const colvec& Y, const colvec& lfactY, const mat& X, const mat
 	                 temp + Delta * (K * eta + rvFi * (gr % b)) - l0u * (kron(exp(K * eta), exp(repmat(Fu, 1, nK) * (gr % b)))));
 }
 
-// Gradient function
+// Gradient function - NB some redundant arguments are included to allow for ease of use in ucminf
 // [[Rcpp::export]]
-colvec gradll(vec& b, const colvec& Y, const mat& X, const mat& Z, const mat& D,
-		     const rowvec& K, const int Delta, const double l0i,
-		     const rowvec& l0u, const mat& Fu, const vec& beta, const vec& eta,
-		     const vec& gr, const rowvec& rvFi, const int nK){
+colvec gradll(vec& b, const colvec& Y, const colvec& lfactY, const mat& X, const mat& Z, const mat& D,
+		  const rowvec& K, const int Delta, const double l0i, const rowvec& Fi,
+		  const rowvec& l0u, const mat& Fu, const vec& beta, const vec& eta,
+		  const vec& gr, const rowvec& rvFi, const int nK, const int q){
 	return -1.0 * (-Z.t() * exp(X * beta + Z * b) + Z.t() * Y - D.i() * b + Delta * rvFi.t() % gr + 
 	                 - repmat(Fu, 1, nK).t() * (l0u.t() % (kron(exp(K * eta), exp(repmat(Fu, 1, nK) * (gr % b))))) % gr);
 
+}
+
+// Just functions for the poisson likelihood
+// [[Rcpp::export]]
+double po_ll(vec& b, const colvec& Y, const colvec& lfactY, const mat& X, const mat& Z,
+				const mat& D, const vec& beta, const int q){
+	return -1.0 * as_scalar(
+		sum(-lfactY) + sum(-exp(X * beta + Z * b)) + Y.t() * (X * beta + Z * b) + 
+		-q/2.0 * log(2.0 * M_PI) - 0.5 * log(det(D)) - 0.5 * b.t() * D.i() * b
+	);
+}
+
+// Poisson first derivative wrt bi
+// [[Rcpp::export]]
+colvec po_grad(vec& b, const colvec& Y, const colvec& lfactY, const mat& X, const mat& Z,
+				const mat& D, const vec& beta, const int q){
+	return -1.0 * (-Z.t() * exp(X * beta + Z * b) + Z.t() * Y - D.i() * b);
+}
+
+// Poisson second derivate wrt bi
+// [[Rcpp::export]]
+mat po_hess(vec& b, const mat& X, const mat& Z, const mat& D, const vec& beta){
+	vec temp = exp(X * beta + Z * b);
+	mat dmat = diagmat(temp);
+	return -1.0 * ((dmat * Z).t() * Z) - D.i();
 }
