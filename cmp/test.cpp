afc351f9afe6e864bd7838734e4c3cff552bbb88
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-01-18 17:15:53 +0000

    To start looking at Mean parameterised (fns for lambda etc.)

--- cmp/test.cpp
+++ cmp/test.cpp
@@ -15,30 +15,33 @@ arma::vec SEQ_Z(long double summax){ // Shorthand for Z_(lambda_i, nu_i)
 }
 
 // [[Rcpp::export]]
-vec logZ(vec& eta, vec& nu, const int summax){
-	vec ms = SEQ_Z(summax);
-  // vec lms = lfactorial(ms);
-	vec out = vec(eta.size());
-	for(int i = 0; i < eta.size(); i++){
-		for(int j = 1; j < ms.size(); j++){
-			out[i] += eta[i] - nu[i] * lgamma(ms[j] + 1.0);
-		}
-	}
-	return out;
+arma::vec SEQ_Z_2(long double summax){ // Shorthand for Z_(lambda_i, nu_i)
+  return arma::linspace(1, summax, summax);
 }
 
 // [[Rcpp::export]]
-vec Z_(vec& eta, vec& nu, const int summax){
-  vec ms = SEQ_Z(summax);
-  // vec lms = lfactorial(ms);
+vec calc_logZ(vec& eta, vec& nu, int summax){
+  vec ns  = SEQ_Z(summax); // Initialise vector of ns (n = 0, ..., summax)
   vec out = vec(eta.size());
-  for(int i = 0; i < eta.size(); i++){
-    for(int j = 0; j < ms.size(); j++){
-      out[i] += pow(eta[i], ms[j]) / pow(lgamma(ms[j] + 1.0), nu[i]);
+  for(int i = 0; i < eta.size(); i ++){
+    for(int j = 1; j < ns.size(); j++){
+      out[i] += ns[j] * eta[i] - lgamma(ns[j] + 1.0) * nu[i];
     }
+    
   }
-  return out;
-}
+  return out + 1.0; // from cumsum log(0)
+}  
+
+// [[Rcpp::export]]
+vec calc_logZ2(vec& eta, vec& nu, int summax){
+  vec ns  = SEQ_Z_2(summax); // Initialise vector of ns (n = 1, ..., summax)
+  vec lcs = cumsum(log(ns)); // log cumsum
+  vec out = vec(eta.size());
+  for(int i = 0; i < eta.size(); i ++){
+    out[i] = 1.0 + sum(ns * eta[i] - lcs * nu[i]);
+  }
+  return out; // from cumsum log(0)
+}  
 
 // vec dCMP(vec& Y, )
 
