97b22f9c7e29659e78b3de6c6febd4aec4a516c1
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-01-27 16:37:52 +0000

    updates 27/1/22, to move beta,delta stuff to separate program

--- cmp/ll.cpp
+++ cmp/ll.cpp
@@ -35,34 +35,75 @@ arma::vec SEQ_Z(long double summax){ // Shorthand for Z_(lambda_i, nu_i)
   return arma::linspace(0, summax, summax + 1.0);
 }
 
+// credit due to Thomas Fung https://github.com/thomas-fung/mpcmp/blob/main/src/utility.cpp
+// A lot easier to simply work on the log scale
 // [[Rcpp::export]]
-double calcZ_scalar(double lambda, double nu, int summax){
-  double out = 0.0;
-  vec js = SEQ_Z(summax);
-  for(int j = 0; j < js.size(); j++){
-    out += pow(lambda, js[j])/pow(tgamma(js[j] + 1.0), nu);
+vec logZ_c(vec& log_lambda, vec& nu, int summax) {
+  // Control loop
+  // int maxiter = 1e4;
+  double log_epsilon = std::log(1e-10);
+  // Output vector
+  int n = log_lambda.size();
+  vec out = vec(n);
+  // Compute logz
+  for (int i = 0; i < n; ++i) {
+    double logz  = 0;
+    double logz_ = 0;
+    for (int j = 1; j < summax; ++j) {
+      logz_ += log_lambda[i] - nu[i] * log((double)j);
+      logz = R::logspace_add(logz, logz_);
+      if (logz_ - logz < log_epsilon) break;
+    }
+    out[i] = logz;
   }
   return out;
 }
 
 // [[Rcpp::export]]
-vec calcZ(vec& lambda, vec& nu, int summax){
-  vec out = vec(lambda.size());
-  vec js = SEQ_Z(summax);
-  for(int i = 0; i < out.size(); i++){
-    for(int j = 0; j < js.size(); j++){
-      out[i] += pow(lambda[i], js[j])/pow(tgamma(js[j] + 1.0), nu[i]);
-    }
+double logZ_c_scalar(double log_lambda, double nu, int summax) {
+  // Control loop
+  // int maxiter = 1e4;
+  double log_epsilon = std::log(1e-10);
+  // Output vector
+  double out = 0.0;
+  // Compute logz
+  double logz  = 0;
+  double logz_ = 0;
+  for (int j = 1; j < summax; ++j) {
+    logz_ += log_lambda - nu * log((double)j);
+    logz = R::logspace_add(logz, logz_);
+    if (logz_ - logz < log_epsilon) break;
   }
+  out = logz;
   return out;
 }
+
+// double calcZ_scalar(double lambda, double nu, int summax){
+//   double out = 0.0;
+//   vec js = SEQ_Z(summax);
+//   for(int j = 0; j < js.size(); j++){
+//     out += pow(lambda, js[j])/pow(tgamma(js[j] + 1.0), nu);
+//   }
+//   return out;
+// }
+// 
+// vec calcZ(vec& lambda, vec& nu, int summax){
+//   vec out = vec(lambda.size());
+//   vec js = SEQ_Z(summax);
+//   for(int i = 0; i < out.size(); i++){
+//     for(int j = 0; j < js.size(); j++){
+//       out[i] += pow(lambda[i], js[j])/pow(tgamma(js[j] + 1.0), nu[i]);
+//     }
+//   }
+//   return out;
+// }
 
 // Function used to find lambda from supplied mu (see _Functions.R) ----
 
 // [[Rcpp::export]]
 double mu_lambdaZ_eq(double lambda, double mu, double nu, int summax){
   vec js = SEQ_Z(summax);
-  double Z = calcZ_scalar(lambda, nu, summax);
+  double Z = exp(logZ_c_scalar(log(lambda), nu, summax));
   double rhs = 0.0;
   for(int j = 0; j < js.size(); j++){
     rhs += (js[j] * pow(lambda, js[j])) / (pow(tgamma(js[j] + 1.0), nu) * Z);
@@ -74,7 +115,8 @@ double mu_lambdaZ_eq(double lambda, double mu, double nu, int summax){
 
 // [[Rcpp::export]]
 double ll_cmpC(vec& lambda, vec& nu, int summax, vec& Y, vec& lY){
-	vec Z = calcZ(lambda, nu, summax);
+  vec loglambda = log(lambda);
+	vec Z = exp(logZ_c(loglambda, nu, summax));
 	double lhs = as_scalar(Y.t() * log(lambda) - nu.t() * lY);
 	return lhs - sum(Z);
 }
@@ -95,18 +137,90 @@ vec S_ll_b(vec& b, mat& D){
   return -1.0 * D.i() * b;
 }
 
+// Computing Expectations ----------------------------------------------
+// [[Rcpp::export]]
+vec E_means(vec& lambda, vec& nu, vec& logZ, int summax){
+  vec js = SEQ_Z(summax);
+  vec out = vec(lambda.size());
+  for(int i = 0; i < lambda.size(); i++){
+    for(int j = 1; j < js.size(); j++){
+      out[i] += exp(log(js[j] - 1.0) + (js[j] - 1.0) *  log(lambda[i]) - nu[i] * lgamma(js[j]) - logZ[i]);
+    }
+  }
+  return out;
+}
+
+// [[Rcpp::export]]
+vec E_logfactY(vec& lambda, vec& nu, vec& logZ, int summax){
+  vec js = SEQ_Z(summax);
+  vec out = vec(lambda.size());
+  for(int i = 0; i < lambda.size(); i++){
+    for(int j = 1; j < js.size(); j++){
+      out[i] += lgamma(js[j]) * exp((js[j] - 1) * log(lambda[i]) - nu[i] * lgamma(js[j]) - logZ[i]);
+    }
+  }
+  return out;
+}
+
+// [[Rcpp::export]]
+vec E_YlogfactY(vec& lambda, vec& nu, vec& logZ, int summax){
+  vec js = SEQ_Z(summax);
+  vec out = vec(lambda.size());
+  for(int i = 0; i < lambda.size(); i++){
+    for(int j = 1; j < js.size(); j++){
+      out[i] += exp(log(lgamma(js[j])) + log(js[j] - 1.0) + (js[j] - 1.0) * log(lambda[i]) - nu[i] * lgamma(js[j]) - logZ[i]);
+    }
+  }
+  return out;
+}
+
+// Variances -----------------------------------------------------------
+// [[Rcpp::export]]
+vec Var_means(vec& lambda, vec& nu, vec& logZ, int summax){
+  vec js = SEQ_Z(summax);
+  vec out = vec(lambda.size());
+  for(int i = 0; i < lambda.size(); i++){
+    for(int j = 1; j < js.size(); j++){
+      out[i] += exp(2.0 * log(js[j] - 1.0) + (js[j] - 1.0) * log(lambda[i]) - nu[i] * lgamma(js[j]) - logZ[i]);
+    }
+  }
+  return out - pow(E_means(lambda, nu, logZ, summax), 2.0);
+}
+
+// Variance of lfact(Y)
+// [[Rcpp::export]]
+vec Var_logfactY(vec& lambda, vec& nu, vec& logZ, int summax){
+  vec js = SEQ_Z(summax);
+  vec out = vec(lambda.size());
+  for(int i = 0; i < lambda.size(); i++){
+    for(int j = 1; j < js.size(); j++){
+        out[i] += pow(lgamma(js[j]), 2.0) * exp((js[j] - 1.0) * log(lambda[i]) - nu[i] * lgamma(js[j]) - logZ[i]);
+    }
+  }
+  return out - pow(E_logfactY(lambda, nu, logZ, summax), 2.0);
+}
+
 // Score on \beta ------------------------------------------------------
 // Variance V(\mu, Y) 
 
 // [[Rcpp::export]]
 double V_scalar(double mu, double lambda, double nu, int summax){
 	double out = 0.0;
-	double Z = calcZ_scalar(lambda, nu, summax);
+	double Z = exp(logZ_c_scalar(log(lambda), nu, summax));
 	vec js = SEQ_Z(summax);
 	for(int j = 0; j < summax; j++){
 	  out += pow(js[j] - mu, 2.0) * pow(lambda, js[j]) / (pow(tgamma(js[j] + 1.0), nu) * Z);
 	}
 	return out;
+}
+
+// [[Rcpp::export]]
+vec V_mu_lambda(vec& mu, vec& lambda, vec& nu, int summax){
+  vec out(lambda.size());
+  for(int i = 0; i < lambda.size(); i++){
+    out[i] = V_scalar(mu[i], lambda[i], nu[i], summax);
+  }
+  return out;
 }
 
 // A wrapper that uses the above V_scalar and calculates Score(\mu).
@@ -120,12 +234,7 @@ vec Smu(vec& mu, vec& lambda, vec& nu, vec& Y, int summax){
   return (Y - mu) / V; 
 }
 
-// Score on \nu --------------------------------------------------------
-vec Snu(vec& mu, vec& lambda, vec& nu, vec& Y, vec& lY, int summax){
-  vec V  = vec(lambda.size());
-  for(int v = 0; v < V.size(); v++){
-    V[v] = V_scalar(mu[v], lambda[v], nu[v], summax);
-  }
-  return lY.t() * (Y - mu) * 
-}
+
+
+
 
