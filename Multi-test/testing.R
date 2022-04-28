693098b65b08fa3375bd597437cb3505b9d581be
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-04-28 11:26:15 +0100

    new 28/4/22

--- Multi-test/testing.R
+++ Multi-test/testing.R
@@ -9,7 +9,7 @@ long.formulas <- list(
 family <- list(gaussian, gaussian)
 surv.formula <- Surv(survtime, status) ~ drug
 
-my.fit <- EM(long.formulas, surv.formula, pbc, family, control = list(verbose=T, hessian='auto'))
+my.fit <- EM(long.formulas, surv.formula, pbc, family, control = list(hessian='auto'))
 
 mjoint(
   formLongFixed = list(
@@ -80,6 +80,7 @@ my.fit.tri.spline2 <- EM(long.formulas, surv.formula, pbc.tri, family, control =
 
 
 # Five-variate mixture as described in Rustand et al. ---------------------
+library(splines)
 pbc.quin <- pbc[!is.na(pbc$serBilir) & !is.na(pbc$SGOT) & !is.na(pbc$albumin) &
                     !is.na(pbc$platelets) & !is.na(pbc$spiders),]
 pbc.quin$SGOT <- log(pbc.quin$SGOT)
@@ -97,12 +98,39 @@ family <- list(gaussian, gaussian, gaussian, poisson, binomial)
 
 my.rustand <- EM(long.formulas, surv.formula, pbc.quin, family,
                  control = list(hessian = 'auto'))
-my.rustand$totaltime
+source('multivariate-compare.R')
+# load('RUSTANDFIT.RData')
+my.summary(my.rustand)
 
 
+# JMbayes2 Rustand fit ----------------------------------------------------
+library(JMbayes2)
+M1 <- lme(serBilir ~ drug * ns(time, knots = c(1,4)), random = ~ns(time, knots = c(1, 4))|id, data = pbc.quin,
+          method = "ML",
+          control = nlme::lmeControl(opt = "optim", msTol = 1e-3))
+M2 <- lme(SGOT ~ drug * ns(time, knots = c(1,4)), random = ~ns(time, knots = c(1, 4))|id, data = pbc.quin,
+          method = "ML",
+          control = nlme::lmeControl(opt = "optim", msTol = 1e-3))
+M3 <- lme(albumin ~ drug * time, random = ~time|id, data = pbc.quin)
+temp <- glmmTMB(platelets ~ drug * ns(time, knots = c(1,4)) + (1 + ns(time, knots = c(1,4)) | id),
+                data = pbc.quin, family = poisson)
+M4 <- mixed_model(platelets ~ drug * ns(time, knots = c(1, 4)), random = ~ns(time, knots = c(1, 4)) | id,
+                  data = pbc.quin, family = poisson,
+                  initial_values = list(
+                    betas = glmmTMB::fixef(temp)$cond,
+                    D = matrix(glmmTMB::VarCorr(temp)$cond$id, 4, 4)
+                  ))
+M5 <- mixed_model(spiders ~ drug * time, random = ~ time|id, data = pbc.quin, family = binomial)
+Ms <- list(M1, M2, M3, M4, M5)
+survdata <- parseCoxph(surv.formula, pbc.quin)$survdata
+phfit <- coxph(Surv(survtime, status) ~ drug, survdata)
 
-
-
+JMbayes2.rustand <- jm(
+  Surv_object = phfit,
+  Mixed_objects = Ms,
+  time_var = 'time',
+  data_Surv = survdata
+)
 
 
 
