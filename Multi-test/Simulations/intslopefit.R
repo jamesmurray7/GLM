860adb26ed9803b22de0ba96bde192747c965434
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-05-16 09:47:25 +0100

    inla.surv object ->> global environment, otherwise fn wont work!

--- Multi-test/Simulations/intslopefit.R
+++ Multi-test/Simulations/intslopefit.R
@@ -89,17 +89,17 @@ INLAfit <- function(data, k, family){
   long.formulas <- vector('list', k)
   for(kk in 1:k) long.formulas[[kk]] <- as.formula(paste0('Y.', kk, '~ time + cont + bin + (1 + time|id)'))
   survdata <- data[!duplicated(data[, 'id']), ]
-  surv <- inla.surv(time = c(survdata$survtime), event = c(survdata$status))
+  fail <<- inla.surv(time = c(survdata$survtime), event = c(survdata$status)) # send to global env or joint() doesn't work.
   assocs <- as.list(rep('SRE', k))
   JMINLA <- joint(
     formLong = long.formulas,
-    formSurv = surv ~ bin,
+    formSurv = fail ~ bin,
     dataLong = data, dataSurv = survdata,
     id = 'id', timeVar = 'time', corLong = T,
     family = rep(family, k), basRisk = 'rw1',
     assoc = assocs,
     control = list(int.strategy='eb',
-                   priorRandom = list(r = 6, R = 1),
+                   priorRandom = list(r = 2*k, R = 1),
                    priorAssoc = list(mean = 0, prec = 0.16),
                    priorFixed = list(mean = 0, prec = 0.16, 
                                      mean.intercept = 0,
@@ -195,6 +195,7 @@ save(fit3.jML, file =  'Simulations/fits/gaussianK-3_joineRML.RData')
 
 # JMbayes2 ----------------------------------------------------------------
 # K=1 JMbayes 2------------------------------------------------------------
+library(JMbayes2)
 data1 <- .loader('Simulations/data/gaussianK-1.RData')
 fit1.JMb <- vector('list', 100)
 pb <- utils::txtProgressBar(max=100,style=3)
@@ -230,22 +231,22 @@ save(fit3.JMb, file = 'Simulations/fits/gaussianK-3_JMbayes2.RData')
 
 # END GAUSSIAN --------
 # Poisson -----------------------------------------------------------------
-rm(data1, data2, data3)
+# rm(data1, data2, data3) already done!
 data1 <- .loader('Simulations/data/poissonK-1.RData')
 data2 <- .loader('Simulations/data/poissonK-2.RData')
 data3 <- .loader('Simulations/data/poissonK-3.RData')
-fit1 <- fit2 <- fit3 <- fit1.JMb <- fit2.JMb <- fit3.JMb <- vector('list', 100)
-pb <- utils::txtProgressBar(max = 100, style = 3)
-for(i in 1:100){
-  d1 <- data1[[i]]; d2 <- data2[[i]]; d3 <- data3[[i]]
-  fit1[[i]] <- emfit(d1, 1, 'poisson')
-  fit2[[i]] <- emfit(d2, 2, 'poisson')
-  fit3[[i]] <- emfit(d3, 3, 'poisson')
-  utils::setTxtProgressBar(pb, i)
-}
-save(fit1, file = 'Simulations/fits/poissonK-1.RData')
-save(fit2, file = 'Simulations/fits/poissonK-2.RData')
-save(fit3, file = 'Simulations/fits/poissonK-3.RData')
+# fit1 <- fit2 <- fit3 <- fit1.JMb <- fit2.JMb <- fit3.JMb <- vector('list', 100)
+# pb <- utils::txtProgressBar(max = 100, style = 3)
+# for(i in 1:100){
+#   d1 <- data1[[i]]; d2 <- data2[[i]]; d3 <- data3[[i]]
+#   fit1[[i]] <- emfit(d1, 1, 'poisson')
+#   fit2[[i]] <- emfit(d2, 2, 'poisson')
+#   fit3[[i]] <- emfit(d3, 3, 'poisson')
+#   utils::setTxtProgressBar(pb, i)
+# }
+# save(fit1, file = 'Simulations/fits/poissonK-1.RData')
+# save(fit2, file = 'Simulations/fits/poissonK-2.RData')
+# save(fit3, file = 'Simulations/fits/poissonK-3.RData')
 
 # JMbayes2
 pb <- utils::txtProgressBar(max = 100, style = 3)
@@ -259,6 +260,47 @@ for(i in 1:100){
 save(fit1.JMb, file = 'Simulations/fits/poissonK-1_JMbayes2.RData')
 save(fit2.JMb, file = 'Simulations/fits/poissonK-2_JMbayes2.RData')
 save(fit3.JMb, file = 'Simulations/fits/poissonK-3_JMbayes2.RData')
+
+
+# INLAjoint ---------------------------------------------------------------
+# INLA
+library(INLA)
+library(INLAjoint)
+inla.setOption(inla.mode="experimental")
+data1 <- .loader('Simulations/data/gaussianK-1.RData')
+data2 <- .loader('Simulations/data/gaussianK-2.RData')
+data3 <- .loader('Simulations/data/gaussianK-3.RData')
+
+pb <- utils::txtProgressBar(max = 100, style = 3)
+fit1.INLA <- fit2.INLA <- fit3.INLA <- vector('list', 100)
+for(i in 1:100){
+  d1 <- data1[[i]]; d2 <- data2[[i]]; d3 <- data3[[i]]
+  fit1.INLA[[i]] <- INLAfit(d1, 1, 'gaussian')
+  fit2.INLA[[i]] <- INLAfit(d2, 2, 'gaussian')
+  fit3.INLA[[i]] <- INLAfit(d3, 3, 'gaussian')
+  utils::setTxtProgressBar(pb, i)
+}
+save(fit1.INLA, file = 'Simulations/fits/gaussianK-1_INLA.RData')
+save(fit2.INLA, file = 'Simulations/fits/gaussianK-2_INLA.RData')
+save(fit3.INLA, file = 'Simulations/fits/gaussianK-3_INLA.RData')
+
+# Poisson
+data1 <- .loader('Simulations/data/poissonK-1.RData')
+data2 <- .loader('Simulations/data/poissonK-2.RData')
+data3 <- .loader('Simulations/data/poissonK-3.RData')
+
+pb <- utils::txtProgressBar(max = 100, style = 3)
+fit1.INLA <- fit2.INLA <- fit3.INLA <- vector('list', 100)
+for(i in 1:100){
+  d1 <- data1[[i]]; d2 <- data2[[i]]; d3 <- data3[[i]]
+  fit1.INLA[[i]] <- INLAfit(d1, 1, 'poisson')
+  fit2.INLA[[i]] <- INLAfit(d2, 2, 'poisson')
+  fit3.INLA[[i]] <- INLAfit(d3, 3, 'poisson')
+  utils::setTxtProgressBar(pb, i)
+}
+save(fit1.INLA, file = 'Simulations/fits/poissonK-1_INLA.RData')
+save(fit2.INLA, file = 'Simulations/fits/poissonK-2_INLA.RData')
+save(fit3.INLA, file = 'Simulations/fits/poissonK-3_INLA.RData')
 
 # END COUNTS --------
 # Binary ------------------------------------------------------------------
