4f034d87ffc646eed47a81cb3bca88e9da4d7e3d
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-05-24 11:44:07 +0100

    All done (v1, provisionally), combining plots  and tidying

--- Multi-test/Simulations/intslopePlot.R
+++ Multi-test/Simulations/intslopePlot.R
@@ -39,7 +39,7 @@ elapsed.times %>%
   mutate_at('K', parse_number) %>% 
   mutate(
     s = ifelse(K == 1, '', 's'),
-    K_label = paste0('K = ', K, ' Gaussian response', s)
+    K_label = paste0('K = ', K)#, ' Gaussian response', s)
   ) %>% 
   ggplot(aes(x = K_label, y = log10(elapsed))) + 
   geom_boxplot(outlier.alpha = .50) + 
@@ -47,9 +47,9 @@ elapsed.times %>%
   stat_summary(fun=median, geom="line", aes(group=1), lty = 5, alpha = .5) +
   stat_summary(fun=median, geom="point", size = 2) + 
   facet_wrap(~ method, scales = 'free_y') +
-  theme_light() + 
+  theme_csda() + 
   labs(
-    x = NULL, y = 'Elapsed time (seconds, log10)'
+    x = NULL, y = expression(underline(bold(Gaussian)))
   ) -> Gplot
 
 #' Poisson
@@ -79,20 +79,18 @@ elapsed.times %>%
   mutate_at('K', parse_number) %>% 
   mutate(
     s = ifelse(K == 1, '', 's'),
-    K_label = paste0('K = ', K, ' count response', s)
+    K_label = paste0('K = ', K)#, ' count response', s)
   ) %>% 
   ggplot(aes(x = K_label, y = log10(elapsed))) + 
   geom_boxplot() + 
+  stat_summary(fun=median, geom="line", aes(group=1), lty = 5, alpha = .5) +
+  stat_summary(fun=median, geom="point", size = 2) + 
   facet_wrap(~ method, scales = 'free_y') +
   # scale_y_log10() +
-  theme_light() + 
   labs(
-    x = NULL, y = 'Elapsed time (seconds, log10)'
+    x = NULL, y = expression(underline(bold(Poisson)))
   ) + 
-  theme(
-    strip.background = element_blank(),
-    strip.text = element_text(colour = 'black', size = 13)
-  ) -> Pplot
+  theme_csda() -> Pplot
 
 #' Binomial
 rm(list = ls()[grepl('fit',ls())])
@@ -101,6 +99,41 @@ for(file in dir(dataDir, '^binomial')){ # load in all data -> Gaussian
   load(paste0(dataDir, '/', file))
 }
 
+aEM.times <- data.frame(method = 'Approximate EM',
+                        `K1` = do.call(c, lapply(fit1, elapsed.aEM)),
+                        `K2` = do.call(c, lapply(fit2, elapsed.aEM)),
+                        `K3` = do.call(c, lapply(fit3, elapsed.aEM)))
+JMb.times <- data.frame(method = 'JMbayes2',
+                        `K1` = do.call(c, lapply(fit1.JMb, elapsed.JMb)),
+                        `K2` = do.call(c, lapply(fit2.JMb, elapsed.JMb)),
+                        `K3` = do.call(c, lapply(fit3.JMb, elapsed.JMb)))
+inj.times <- data.frame(method = 'INLAjoint',
+                        `K1` = do.call(c, lapply(fit1.INLA, elapsed.INLA)),
+                        `K2` = do.call(c, lapply(fit2.INLA, elapsed.INLA)),
+                        `K3` = do.call(c, lapply(fit3.INLA, elapsed.INLA)))
+
+elapsed.times <- rbind(aEM.times, JMb.times, inj.times)
+
+elapsed.times %>% 
+  pivot_longer(K1:K3, names_to = 'K', values_to = 'elapsed') %>% 
+  mutate_at('K', parse_number) %>% 
+  mutate(
+    s = ifelse(K == 1, '', 's'),
+    K_label = paste0('K = ', K)#, ' count response', s)
+  ) %>% 
+  ggplot(aes(x = K_label, y = log10(elapsed))) + 
+  geom_boxplot() + 
+  stat_summary(fun=median, geom="line", aes(group=1), lty = 5, alpha = .5) +
+  stat_summary(fun=median, geom="point", size = 2) + 
+  facet_wrap(~ method, scales = 'free_y') +
+  labs(
+    x = NULL, y = expression(underline(bold(Binomial)))
+  ) + 
+  theme_csda() -> Bplot
+
+#' Combine?
+all.plots <- ggpubr::ggarrange(Gplot,Pplot,Bplot,ncol=1)
+ggpubr::annotate_figure(all.plots, left = 'Elapsed time (seconds, log10)')
 # Plotting Estimates ------------------------------------------------------
 
 #' Gaussian
@@ -131,7 +164,7 @@ for(file in dir(dataDir, '^binom')){ # load in all data -> Gaussian
   cat(paste0(file, '\n'))
   load(paste0(dataDir, '/', file))
 }
-plot.ests(fit1, NULL, NULL, fit1.INLA, f = 'binomial', K = 1)
-plot.ests(fit2, NULL, NULL, fit2.INLA, f = 'binomial', K = 2)
-plot.ests(fit3, NULL, NULL, NULL, f = 'binomial', K = 3)
+plot.ests(fit1, NULL, fit1.JMb, fit1.INLA, f = 'binomial', K = 1)
+plot.ests(fit2, NULL, fit2.JMb, fit2.INLA, f = 'binomial', K = 2)
+plot.ests(fit3, NULL, fit3.JMb, fit3.INLA, f = 'binomial', K = 3)
 
