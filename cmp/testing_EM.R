564860dcd3d897c19d182d7940e2cbc78ebacfd7
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-06-13 15:57:49 +0100

    fleshing out testing for EM

--- cmp/testing_EM.R
+++ cmp/testing_EM.R
@@ -4,8 +4,10 @@ long.formula <- Y~time+cont+bin+(1+time|id)
 surv.formula <- Surv(survtime, status) ~ cont + bin
 disp.formula <- ~time
 
-data <- simData_joint(n = , delta = c(-0.5, 0.1), ntms = 15, theta = c(-3, .25))
+data <- simData_joint(n = 100, delta = c(-0.5, 0.1), ntms = 15, theta = c(-3, .25))
 data <- data$data
 
-test <- EM(long.formula, disp.formula, surv.formula, data, control = list(verbose = T, summax.override = T, tol = 5e-2, gh.nodes = 3
-                                                                          ),summax=100)
+test <- EM(long.formula, disp.formula, surv.formula, data, 
+           control = list(verbose = T, summax.override = T, tol = 5e-2, gh.nodes = 3),
+           summax=100)
+plot.stepmat(test)
